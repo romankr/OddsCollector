2a6fd6531cd399f1414ff9033704ee65b0182828
romankr <romankrasilnikov@yandex.ru>
2024-11-17 14:05:17 +0400

    Add Host integrity test

---
+++ OddsCollector.Functions.Tests/Tests/HostProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace OddsCollector.Functions.Tests.Tests;
+
+internal class HostProvider
+{
+    [Test]
+    public void Get_ReturnsHost()
+    {
+        var host = OddsCollector.Functions.HostProvider.Get();
+
+        host.Should().NotBeNull();
+    }
+}
---
+++ OddsCollector.Functions/HostProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OddsCollector.Functions.OddsApi.Configuration;
+using OddsCollector.Functions.Processors.Configuration;
+using OddsCollector.Functions.Strategies.Configuration;
+
+namespace OddsCollector.Functions;
+
+internal static class HostProvider
+{
+    public static IHost Get()
+    {
+        return new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .ConfigureServices(services =>
+            {
+                services.AddPredictionStrategy();
+                services.AddFunctionProcessors();
+                services.AddApplicationInsightsTelemetryWorkerService();
+                services.ConfigureFunctionsApplicationInsights();
+                services.AddOddsApiClientWithDependencies(
+                    Environment.GetEnvironmentVariable("OddsApiClient:Leagues"),
+                    Environment.GetEnvironmentVariable("OddsApiClient:ApiKey"));
+            })
+            .Build();
+    }
+}
