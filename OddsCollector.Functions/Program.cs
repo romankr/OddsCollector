f75fe0eda8a94c15a8bbaee71c7658f4f967ea2d
romankr <romankrasilnikov@yandex.ru>
2024-11-17 21:16:34 +0400

    Add Host integrity test (#60)

--- OddsCollector.Functions/Program.cs
+++ OddsCollector.Functions/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OddsCollector.Functions.OddsApi.Configuration;
-using OddsCollector.Functions.Processors.Configuration;
-using OddsCollector.Functions.Strategies.Configuration;
 
 [assembly: InternalsVisibleTo("OddsCollector.Functions.Tests")]
 // DynamicProxyGenAssembly2 is a temporary assembly built by mocking systems that use CastleProxy
@@ -16,20 +11,6 @@ internal static class Program
 {
     private static void Main()
     {
-        var host = new HostBuilder()
-            .ConfigureFunctionsWorkerDefaults()
-            .ConfigureServices(services =>
-            {
-                services.AddPredictionStrategy();
-                services.AddFunctionProcessors();
-                services.AddApplicationInsightsTelemetryWorkerService();
-                services.ConfigureFunctionsApplicationInsights();
-                services.AddOddsApiClientWithDependencies(
-                    Environment.GetEnvironmentVariable("OddsApiClient:Leagues"),
-                    Environment.GetEnvironmentVariable("OddsApiClient:ApiKey"));
-            })
-            .Build();
-
-        host.Run();
+        HostProvider.Get().Run();
     }
 }
