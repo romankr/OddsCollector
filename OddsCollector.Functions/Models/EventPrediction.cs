38bb744eee822ab70d5c28ded21388499c16ad78
romankr <romankrasilnikov@yandex.ru>
2025-02-10 21:25:38 +0400

    Fix comments

--- OddsCollector.Functions/Models/EventPrediction.cs
+++ OddsCollector.Functions/Models/EventPrediction.cs
@@ -4,16 +4,13 @@ namespace OddsCollector.Functions.Models;
 
 internal sealed class EventPrediction
 {
-    // duplicating information to avoid complex cosmosdb queries
     public string AwayTeam { get; set; } = string.Empty;
 
-    // duplicating information to avoid complex cosmosdb queries
     public DateTime CommenceTime { get; set; } = DateTime.MinValue;
 
-    // duplicating information to avoid complex cosmosdb queries
     public string HomeTeam { get; set; } = string.Empty;
 
-    // fixed id for cosmosdb
+    // cosmosdb requires lowercase
     [JsonPropertyName("id")] public string Id { get; set; } = string.Empty;
     public string Winner { get; set; } = string.Empty;
 }
