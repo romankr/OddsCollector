02ff383a0afd0a45b08e13811d0a5a1f663633f0
romankr <romankrasilnikov@yandex.ru>
2025-02-10 21:59:03 +0400

    Exclude classes from code coverage

---
+++ OddsCollector.Functions/OddsApi/WebApi/ApiException.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace OddsCollector.Functions.OddsApi.WebApi;
+
+[ExcludeFromCodeCoverage]
+public partial class ApiException
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class ApiException<TResult>
+{
+}
